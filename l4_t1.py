104ac6b75d1c7768fe413a8d06031dcda03ef412
ankitkumarpatel <ankitkumar.patel@dal.ca>
2019-02-04 12:14:51 -0800

    fixed error of importing string module

--- l4_t1.py
+++ l4_t1.py
@@ -1,6 +1,7 @@
 """
 Write a program that reads a file, breaks each line into words, strips whitespace and punctuation from the words, and converts them to lowercase.
 """
+import string 
 
 file=open("mybook.txt")
 

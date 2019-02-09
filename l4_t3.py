87f5e9d824dd0dcf108d6b814bac20f99e4a0bea
ankitkumarpatel <ankitkumar.patel@dal.ca>
2019-02-09 14:23:14 -0800

    printed 20 most commonly used words in text

--- l4_t3.py
+++ l4_t3.py
@@ -4,3 +4,29 @@ Modify the program from the previous exercise to print the 20 most frequently-us
 
 
 """
+
+
+import string
+
+file=open("mybook.txt")
+
+#count=0
+hist = dict()
+lst = []
+for line in file:
+    line = line.split()
+    for word in line:
+        word = word.strip(string.punctuation+string.whitespace).lower()
+        #count +=1
+        hist[word] = hist.get(word,0)+1
+
+       # print(word)
+print("Total number of words are %d" % sum(hist.values()))
+#print(count)
+
+print("Total number of different words are %d" % len(hist))
+
+lst = [(value,key)for key,value in hist.items()]
+lst.sort(reverse=True)
+for item in lst[:20]:
+    print(item)

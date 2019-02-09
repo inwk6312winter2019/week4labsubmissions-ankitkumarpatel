f70c7c46f8001a091833c5dfce0644bf190330ca
ankitkumarpatel <ankitkumar.patel@dal.ca>
2019-02-09 14:29:54 -0800

    added word file for word operations on book

--- l4_t4.py
+++ l4_t4.py
@@ -3,3 +3,4 @@ Modify the previous program to read a word list and then print all the words in 
 Python provides a data structure called set that provides many common set operations. Read the documentation and write a program that uses set subtraction to find words in the book that are not in the word list.
 
 """
+

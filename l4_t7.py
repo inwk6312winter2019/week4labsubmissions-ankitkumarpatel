9fae8b1e299fed36eacb5fc6e72112fa5bf27284
ankitkumarpatel <ankitkumar.patel@dal.ca>
2019-02-09 18:59:27 -0800

    replaced pattern with replacement string and debugged error of string assignmet by adding print statement in for loop

--- l4_t7.py
+++ l4_t7.py
@@ -3,3 +3,21 @@ Write a function called sed  that takes as arguments a pattern string, a replace
 If an error occurs while opening, reading, writing, or closing files, your program should catch the exception, print an error message, and exit.
 
 """
+def sed(pattern,replacement,fileread,filewrite):
+    try:
+        fin = open(fileread,'r')
+        fout = open(filewrite, 'w')
+        for line in fin:
+            if pattern in line:
+                print(line)
+                line = line.replace(pattern,replacement)
+                print(line)
+            fout.write(line)
+    except:
+        print("Something wrong happenned while replacement!!!")
+    finally:
+        print("Good bye!!!")
+
+
+
+sed('The','WOW','smallbook.txt','temp.txt')

286ea830e7ff9178d9bab1d977c319c1c77b3418
ankitkumarpatel <ankitkumar.patel@dal.ca>
2019-02-09 18:14:28 -0800

    explored OS module and used walk method to pint alll the sub folders and files in the particular directory

--- l4_t6.py
+++ l4_t6.py
@@ -2,3 +2,15 @@
 The os module provides a function called walk that is similar to one below but more versatile. Read the documentation and use it to print the names of the files in a given directory and its subdirectories.
 
 """
+import os
+
+os.chdir("/home/student/Documents/ankit")
+
+for root,dirs,files in os.walk(".",topdown = False):
+
+    for name in files:
+        print(os.path.join(root,name))
+
+
+    for name in dirs:
+        print(os.path.join(root,name))

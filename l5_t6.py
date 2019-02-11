c22afdc1bc76355c3233cba5890b0e0df6e3aa6c
ankitkumarpatel <ankitkumar.patel@dal.ca>
2019-02-10 19:51:29 -0800

    Addition of two point objects and between tuple and point object performed

--- l5_t6.py
+++ l5_t6.py
@@ -6,3 +6,27 @@ Write an add method for Points that works with either a Point object or a tuple:
 
 
 """
+class Point():
+    def __init__(self,x=0,y=0):
+        self.x = x
+        self.y = y
+
+    def __str__(self):
+        return (f"{self.x},{self.y}")
+
+    def __add__(self, other):
+        return (self.x+other.x,self.y+other.y)
+
+
+    def add(self, other):
+        if isinstance(other, tuple):
+            return (self.x + other[0], self.y + other[1])
+        else:
+            return self + other
+
+center = Point(5,10)
+corner = Point(10,8)
+tup=(10,8)
+
+print("sum of two point objects is:",center.add(corner))
+print("sum of point object and tuple is:",center.add(tup))

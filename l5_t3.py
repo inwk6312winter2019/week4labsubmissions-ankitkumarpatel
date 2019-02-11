ae05e9b8427ef1b911dc611df916feb8b33ad4db
ankitkumarpatel <ankitkumar.patel@dal.ca>
2019-02-10 18:41:32 -0800

    wrote new class for Time and new function to print it

--- l5_t3.py
+++ l5_t3.py
@@ -3,3 +3,18 @@
 Write a function called print_time that takes a Time object and prints it in the form hour:minute:second. Hint: the format sequence '%.2d'prints an integer using at least two digits, including a leading zero if necessary. Bonus: Try to use the “format” method of String objects
 
 """
+
+
+class Time:
+    def __init__(self,hour,minute,second):
+        self.hour = hour
+        self.minute = minute
+        self.second = second
+
+
+def print_time(t):
+
+    print('%.2d:%.2d:%.2d'.format(t.hour, t.minute, t.second))
+
+time = Time(10,10,10)
+print_time(time)

90750faf12cee13157e0a341275ffefe441adc79
ankitkumarpatel <ankitkumar.patel@dal.ca>
2019-02-10 17:20:48 -0800

    created files for each task in lab 5

---
+++ l5_t1.py
@@ -0,0 +1,33 @@
+"""
+
+Create a new class called Point. This class will have a “x” and “y” attribute.
+write a function called distance_between_points that takes two Points as arguments and returns the distance between them.
+Test you function by instantiating two instances and assigning them x and y attributes of type Int
+
+
+"""
+
+from math import sqrt
+
+class Point():
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+
+def distance(p,q):
+    distance = sqrt((p.x-q.x)**2+(p.y-q.y)**2)
+    return(f"distance between given two points is {distance}.")
+
+
+x = int(input("Enter x coordinate of point a: "))
+y = int(input("Enter y coordinate of point a: "))
+a = Point(x,y)
+
+x = int(input("Enter x coordinarte of point b: "))
+y = int(input("Enter y coordinarte of point b: "))
+b = Point(x,y)
+
+print(distance(a,b))
+
+
+

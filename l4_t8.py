a86d9e9a005b86e3a657340adfb5dbe4ed986d99
ankitkumarpatel <ankitkumar.patel@dal.ca>
2019-02-10 16:12:23 -0800

    printed all files and folders in week4lab submission directory

--- l4_t8.py
+++ l4_t8.py
@@ -6,3 +6,15 @@ In a large collection of MP3 files, there may be more than one copy of the same 
 
 """
 
+import os
+
+os.chdir("/home/student/week4labsubmissions-ankitkumarpatel")
+
+for root,dirs,files in os.walk(".",topdown = False):
+
+    for name in files:
+        print(os.path.join(root,name))
+
+
+    for name in dirs:
+        print(os.path.join(root,name))

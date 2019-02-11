a34136cdb8ff4c5babd1a17e0f5f140c73c7cd60
ankitkumarpatel <ankitkumar.patel@dal.ca>
2019-02-10 16:50:30 -0800

    wrote function to find duplicate mp3 files and print them

--- l4_t8.py
+++ l4_t8.py
@@ -8,10 +8,38 @@ In a large collection of MP3 files, there may be more than one copy of the same 
 
 import os
 
-os.chdir("/home/student/week4labsubmissions-ankitkumarpatel")
-mp3_list = []
-for root,dirs,files in os.walk(".",topdown = False):
 
-    for name in files:
-        if name[-4:] = ".mp3":
-        mp3_list.append((os.path.join(root,name)))
+
+def get_mp3_files(dirname):
+
+    mp3_list = []
+    for root,dirs,files in os.walk(dirname):
+
+        for name in files:
+            if name[-4:] == ".mp3":
+            mp3_list.append((os.path.join(root,name)))
+
+    return (mp3_list)
+
+def check_duplicates(mp3_list):
+
+    checksum_dict = {}
+
+    for filename in mp3_list:
+        checksum = open('md5sum ' + filename)
+        res = checksum.read()
+        checksum.close()
+
+        if res in checksum_dict:
+            print("Duplicate exists:")
+            print(f"{filename}/{checksum_dict[res]}")
+
+        else:
+
+            checksum_dict[res] = filename
+
+
+
+
+dirname = "/home/student/week4labsubmissions-ankitkumarpatel"
+check_duplicates(get_mp3_files(dirname))
